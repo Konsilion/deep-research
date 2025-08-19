29dc77a66a8c778b2c88b4192f50e7444e972e48
nicolasbremond <nicolas.bremond@konsilion.fr>
2025-08-19 15:45:55 +0000

    IntroModel

--- src/feedback.ts
+++ src/feedback.ts
@@ -1,9 +1,7 @@
 import { generateObject } from 'ai';
 import { z } from 'zod';
 
-import { getModel } from './ai/providers';
-import { getIntroductionModel } from './ai/providers';
-
+import { getIntroductionModel, getModel } from './ai/providers';
 
 import { systemPrompt } from './prompt';
 
@@ -18,6 +16,7 @@ export async function generateFeedback({
 }) {
   const model = introduction ? getIntroductionModel() : getModel();
   const userFeedback = await generateObject({
+    model: model,
     system: systemPrompt(),
     prompt: `
 À partir de la requête suivante, pose DES QUESTIONS DE SUIVI EN FRANÇAIS pour clarifier la direction de la recherche.
